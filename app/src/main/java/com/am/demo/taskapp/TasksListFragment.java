c58cd7db10eac1d1c752d5f563a01fab4b148471
malbor806 <malbor806@gmail.com>
2017-04-23 20:52:55 +0200

    init

---
+++ app/src/main/java/com/am/demo/taskapp/TasksListFragment.java
@@ -0,0 +1,29 @@
+package com.am.demo.taskapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TasksListFragment extends Fragment {
+
+
+    public TasksListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tasks_list, container, false);
+    }
+
+}
